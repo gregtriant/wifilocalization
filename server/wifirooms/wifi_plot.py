e96ae1214d7a17e2b34df059b574922c03d5b3e1
DELL_GREG\gregtriant <greg.triant@outlook.com>
2022-09-02 15:07:14 +0300

    plot for single point

--- server/wifirooms/wifi_plot.py
+++ server/wifirooms/wifi_plot.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import statistics
 
-# def p2f(x):  # x is type string
-#     return float(x.strip('%')) / 100
+show_dbm_of_scan = True
 
 
 class SignalPoint:
@@ -45,7 +44,7 @@ for index, point in enumerate(points):
 print('Found ', len(signal_points), 'different points')
 
 # chose a point
-signal_point = signal_points[120]
+signal_point = signal_points[66]
 
 print(signal_point.x, signal_point.y)
 
@@ -62,6 +61,7 @@ for index, scan in enumerate(signal_point.scans):
 print(unique_bssids)
 
 # for each unique bssid get the ssi level and plot it against time (40 scans)
+chosen_ssids = []
 for unique_net in unique_bssids:
     bssid = unique_net[0]
     ssid = unique_net[1]
@@ -74,30 +74,45 @@ for unique_net in unique_bssids:
 
     freq = len(signal_strengths) / len(signal_point.scans)  # number of times this bssid appeared during the 40 scans
     print(bssid, ssid, ' freq:', "{:6.4f}".format(freq), signal_strengths)
-    if freq >= 0.8:
-        plt.figure()
+    if freq >= 0.7:
+        chosen_ssids.append(ssid)
+        if show_dbm_of_scan:
+            plt.figure()
 
-        # plot mean
-        dbm_vals = [x[1] for x in signal_strengths]
-        print(dbm_vals)
-        mean = np.mean(dbm_vals)
-        std = np.std(dbm_vals)
-        var = np.var(dbm_vals)
-        mode = statistics.mode(dbm_vals)  # mode is the most frequent value of the array
-        mean_arr = np.repeat(mean, 40)
-        x_val = np.arange(0, 40, 1)
-        plt.plot(x_val, mean_arr, color="r", linewidth=1, linestyle="dotted")
-        plt.figtext(.15, .15, "Mean: " + str(format(mean, ".2f")) +
-                    "\nVar: " + str(format(var, ".2f")) +
-                    "\nStd: " + str(format(std, ".2f")) +
-                    "\nMode: " + str(mode))
-        # plot all the points
-        plt.scatter(*zip(*signal_strengths), s=10)
-        plt.xlim(-1, 41)
-        plt.ylim(-100, -45)
-        plt_title = "Levels for: " + bssid + " " + ssid
-        plt.title(plt_title)
-        plt.xlabel("Scan Number")
-        plt.ylabel("dbm level")
+            # plot mean
+            dbm_vals = [x[1] for x in signal_strengths]  # get second value of tuples
+            # print(dbm_vals)
+            mean = np.mean(dbm_vals)
+            std = np.std(dbm_vals)
+            var = np.var(dbm_vals)
+            mode = statistics.mode(dbm_vals)  # mode is the most frequent value of the array
+            mean_arr = np.repeat(mean, 40)
+            mode_arr = np.repeat(mode, 40)
+            x_val = np.arange(0, 40, 1)
+            plt.plot(x_val, mean_arr, color="r", linewidth=1, linestyle="dotted")
+            plt.plot(x_val, mode_arr, color="g", linewidth=1, linestyle="dotted")
+            plt.figtext(.15, .15, "Mean: " + str(format(mean, ".2f")) +
+                        "\nVar: " + str(format(var, ".2f")) +
+                        "\nStd: " + str(format(std, ".2f")) +
+                        "\nMode: " + str(mode))
+            # plot all the points
+            # plt.scatter(*zip(*signal_strengths), s=10)
+            plt.plot(*zip(*signal_strengths), marker='.', markersize=7, linewidth=1)
+            plt.xlim(-1, 41)
+            plt.ylim(-100, -35)
+            plt_title = "Levels for: " + bssid + " " + ssid
+            plt.title(plt_title)
+            plt.xlabel("Scan Number")
+            plt.ylabel("dBm level")
+            plt.legend(['mean', 'mode'], loc='best')
+
+            # collective Figure
+            plt.figure(100)
+            plt.plot(*zip(*signal_strengths), marker='.', markersize=7, linewidth=1)
+            plt.xlim(-1, 41)
+            plt.ylim(-100, -35)
+            plt.xlabel("Scan Number")
+            plt.ylabel("dBm level")
+            plt.legend(chosen_ssids, loc='best')
 
 plt.show()
